8588ceb93bdaa2610315b6a40c4232cabf28cc49
zafar <ertaevzafar@gmail.com>
2024-08-31 13:26:30 +0500

    save3

--- bot.js
+++ bot.js
@@ -1,16 +1,23 @@
+import { Telegraf } from 'telegraf';
 
-import { Telegraf } from "telegraf";
-const TOKEN = "7462210652:AAFFkIFBl9zTAf4ZXC9vaqeW3QqQKTG568I";
+const TOKEN = '7462210652:AAFFkIFBl9zTAf4ZXC9vaqeW3QqQKTG568I';
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://telegram-app-gamma.vercel.app/";
+const webLink = 'https://telegram-app-gamma.vercel.app/';
 
-bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+bot.start((ctx) => {
+  ctx.reply('Welcome :)))))', {
     reply_markup: {
-      keyboard: [[{ text: "web app dummy", web_app: { url: web_link } }]],
-    },
-  })
-);
+      inline_keyboard: [
+        [
+          {
+            text: 'Open Web App',
+            web_app: { url: webLink }
+          }
+        ]
+      ]
+    }
+  });
+});
 
 bot.launch();
--- src/pages/ZodiacPage.jsx
+++ src/pages/ZodiacPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ZodiacById from '../components/ZodiacById/ZodiacById'
 import { useParams} from "react-router-dom"
 import { useZodiaById } from '../hooks/useZodiacById'
@@ -8,6 +8,19 @@ const ZodiacPage = () => {
   const params = useParams()
 
   const { item } = useZodiaById(params.id)
+
+
+  useEffect(() => {
+    // Разворачиваем приложение на весь экран
+    const tg = window.Telegram.WebApp
+
+    tg.BackButton.isVisible = true
+    tg.BackButton.show()
+    
+        
+  
+
+}, []);
 
   return (
     <>
