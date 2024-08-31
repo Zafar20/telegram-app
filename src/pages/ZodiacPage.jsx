ad0c491d4a4a711c6b47636d342a13ebbf8ad457
Zafar20 <60092029+Zafar20@users.noreply.github.com>
2024-08-31 13:35:01 +0500

    Update ZodiacPage.jsx

--- src/pages/ZodiacPage.jsx
+++ src/pages/ZodiacPage.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import ZodiacById from '../components/ZodiacById/ZodiacById'
-import { useParams} from "react-router-dom"
+import { useNavigate, useParams} from "react-router-dom"
 import { useZodiaById } from '../hooks/useZodiacById'
 
 const ZodiacPage = () => {
 
   const params = useParams()
+  const navigate = useNavigate()
 
   const { item } = useZodiaById(params.id)
 
@@ -17,7 +18,8 @@ const ZodiacPage = () => {
     tg.BackButton.isVisible = true
     tg.BackButton.show()
     
-        
+    tg.onEvent('backButtonClicked',() =>  navigate('/'))
+
   
 
 }, []);
