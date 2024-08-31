bd4185bca29e2100eda414d6f781af791adbb39d
Zafar20 <60092029+Zafar20@users.noreply.github.com>
2024-08-31 13:40:57 +0500

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -13,6 +13,19 @@ const MainPage = () => {
     if (tg.initDataUnsafe?.user?.language_code) {
       setLanguage(tg.initDataUnsafe.user.language_code);
     }
+    const savedLanguage = localStorage.getItem('appLanguage');
+    if (savedLanguage) {
+      setLanguage(savedLanguage);
+    }
+
+     // Разворачиваем приложение на весь экран
+    //  const tg = window.Telegram.WebApp;
+     tg.expand();
+
+     // Опционально: можно установить минимальную высоту приложения через стили
+    //  document.body.style.height = '100vh';
+     tg.BackButton.show()
+
   }, []);
 
   useEffect(() => {
@@ -26,16 +39,16 @@ const MainPage = () => {
     localStorage.setItem('appLanguage', language);
   }, [language]);
 
-  useEffect(() => {
-      // Разворачиваем приложение на весь экран
-      const tg = window.Telegram.WebApp;
-      tg.expand();
+//   useEffect(() => {
+//       // Разворачиваем приложение на весь экран
+//       const tg = window.Telegram.WebApp;
+//       tg.expand();
 
-      // Опционально: можно установить минимальную высоту приложения через стили
-      document.body.style.height = '100vh';
-    
+//       // Опционально: можно установить минимальную высоту приложения через стили
+//       document.body.style.height = '100vh';
+//       tg.BackButton.show()
   
-}, []);
+// }, []);
   
 
 
